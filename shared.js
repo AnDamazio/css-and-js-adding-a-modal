2b788e36ab83137bb6e728bab4251d733b408375
AnDamazio <damazioandre@hotmail.com>
2022-07-20 12:28:42 -0300

    Add shared.js file, add modal trigger when a plan is choose

---
+++ shared.js
@@ -0,0 +1,10 @@
+const backdrop = document.querySelector('.backdrop');
+const selectPlanButtons = document.querySelectorAll('.plan button');
+const modal = document.querySelector('.modal');
+
+selectPlanButtons.forEach((element) => {
+    element.addEventListener('click', function () {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    });
+});
