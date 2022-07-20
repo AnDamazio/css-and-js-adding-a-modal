f4a8942abb3fa1e36bc7fcd4c9afba30a649f1e0
AnDamazio <damazioandre@hotmail.com>
2022-07-20 12:53:05 -0300

    close the modal when a button inside this is clicked

--- shared.js
+++ shared.js
@@ -2,6 +2,11 @@ const backdrop = document.querySelector('.backdrop');
 const selectPlanButtons = document.querySelectorAll('.plan button');
 const modal = document.querySelector('.modal');
 
+modal.addEventListener('click', () => {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
+})
+
 selectPlanButtons.forEach((element) => {
     element.addEventListener('click', function () {
         modal.style.display = 'block';
